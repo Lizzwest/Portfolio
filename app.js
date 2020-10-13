e80848daae2330cf599b29e88a320bafcee7cf4c
Lizz <lizwesterband@gmail.com>
2020-10-13 10:18:44 -0700

    add projects to portoflio

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("connected")
+let modal = document.querySelector("#modal")
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+    
